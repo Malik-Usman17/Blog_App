9493ba4b413dd18302e2fce4572d94a74c3fcd5f
malik.usman <malik.usman@int.sybrid.com>
2022-06-08 13:43:43 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import IndexScreen from "./src/screens/IndexScreen";
+import React from "react";
+import { BlogProvider } from "./src/context/BlogContext";
+
+
+const {Screen, Navigator} = createNativeStackNavigator();
+
+
+const App = () => {
+  return(
+    <NavigationContainer>
+      <Navigator initialRouteName="IndexScreen" screenOptions={{headerShown: true, title: 'Blogs'}}>
+        <Screen name={'IndexScreen'} component={IndexScreen}/>
+      </Navigator>
+    </NavigationContainer>
+  )
+}
+
+
+//export default App;
+
+export default () => {
+  return(
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  )
+}
---
+++ src/context/BlogContext.js
@@ -0,0 +1,12 @@
+import React, { createContext } from 'react';
+
+const BlogContext = createContext();
+
+export const BlogProvider = ({ children }) => {
+  return (
+  <BlogContext.Provider value={5}>
+    {children}
+  </BlogContext.Provider>
+  )
+}
+
